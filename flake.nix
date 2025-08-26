e3943cd21d5a7ae17b74958a2843dda6b3c1f514
Mohamed Elsharnouby <melsharnou@chewy.com>
2025-08-26 16:26:48 -0400

    Remove commented instructions for searching packages in flake.nix

--- flake.nix
+++ flake.nix
@@ -10,8 +10,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
         # apps
         alt-tab-macos
